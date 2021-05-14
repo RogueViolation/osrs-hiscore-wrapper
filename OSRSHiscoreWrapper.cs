48acfc4fb2ab8a7fb74c171cda46a80d1008771c
RogueViolation <ignas.rodzianskis@gmail.com>
2021-05-15 00:07:50 +0300

    Add project files.

---
+++ OSRSHiscoreWrapper.cs
@@ -0,0 +1,31 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace osrs_hiscore_wrapper
+{
+    public class OSRSHiscoreWrapper
+    {
+        public OSRSHiscoreWrapper()
+        {
+
+        }
+        public string GetStats(string username, string mode)
+        {
+            var scraper = new HtmlWeb();
+            var uri = BuildUri(username, mode);
+            var html = scraper.Load(uri.ToString());
+
+            return html.Text;
+        }
+
+        public Uri BuildUri(string username, string mode)
+        {
+            var uri = new UriBuilder();
+            uri.Scheme = "https";
+            uri.Host = "secure.runescape.com";
+            uri.Path = OSRSLinks.StatsUrl(OSRSModes.Gamemodes[mode]);
+            uri.Query = OSRSLinks.UrlQuery(username);
+            return uri.Uri;
+        }
+    }
+}
---
+++ OSRSStats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace osrs_hiscore_wrapper
+{
+    class OSRSLevel
+    {
+        private string StatName { get; set; }
+        private int StatLevel { get; set; }
+        private long StatRank { get; set; }
+        private int StatExperience { get; set; }
+    }
+}
