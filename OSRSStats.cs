b599707bb5b3c616d01ad09662bf58669d33e1e9
RogueViolation <ignas.rodzianskis@gmail.com>
2021-05-17 22:35:35 +0300

    Add converter.

--- OSRSStats.cs
+++ OSRSStats.cs
@@ -4,12 +4,28 @@ using System.Text;
 
 namespace osrs_hiscore_wrapper
 {
+    public class OSRSStats
+    {
+        public List<OSRSLevel> Levels { get; set; }
+        public List<OSRSActivity> Activities { get; set; }
+        public List<OSRSBoss> Bosses { get; set; }
+    }
     public class OSRSLevel
     {
         private string StatName { get; set; }
-        private int StatLevel { get; set; }
-        private long StatRank { get; set; }
-        private int StatExperience { get; set; }
+        private string StatRank { get; set; }
+        private string StatLevel { get; set; }
+        private string StatExperience { get; set; }
+
+        public OSRSLevel ToOSRSLevel(string skill, string[] items)
+        {
+            return new OSRSLevel
+            {
+                StatName = skill,
+                StatRank = items[0],
+                StatLevel = items[1],
+                StatExperience = items[2]
+            };
     }
 
     public class OSRSBoss
