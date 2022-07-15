9f955dd51a99b80efb73418a9e5abcf5a4f08e58
RogueViolation <ignas.rodzianskis@gmail.com>
2022-07-16 01:31:42 +0300

    Crude received data mapping to an object

--- OSRSHiscoreWrapper.cs
+++ OSRSHiscoreWrapper.cs
@@ -1,11 +1,15 @@
 ﻿using System.Net.Http;
+using System;
 using osrs_hiscore_wrapper.OSRSWrapper.DataAccess;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace osrs_hiscore_wrapper
 {
     public class OSRSHiscoreWrapper
     {
         public readonly IDataAccess _dataAccess;
+        //public readonly IConfigurationReader _configurationReader;
         public OSRSHiscoreWrapper()
         {
             _dataAccess = new DataAccess();
@@ -13,6 +17,37 @@ namespace osrs_hiscore_wrapper
         public string GetStats(string username, string mode)
         {
             return _dataAccess.GetOSRSStatStringData(username, mode);
+        }
+
+        public OSRSStats ParseStats(string csv)
+        {
+            var stats = new OSRSStats();
+            var parsedCsv = csv.Split('\n').ToList();
+            parsedCsv.RemoveAt(parsedCsv.Count - 1);
+            for (int i = 0; i <= 23; i++)
+            {
+                stats.Levels.Add(new OSRSLevel().ToOSRSLevel(OSRSConst.OSRSSkills[i], parsedCsv[i]));
+            }
+            for (int i = 0; i <= 13; i++)
+            {
+                stats.Activities.Add(new OSRSActivity().ToOSRSActivity(OSRSConst.OSRSActivities[i], parsedCsv[i + 24]));
+            }
+            for (int i = 0; i <= 47; i++)
+            {
+                stats.Bosses.Add(new OSRSBoss().ToOSRSBoss(OSRSConst.OSRSBosses[i], parsedCsv[i + 24 + 13]));
+            }
+
+            return new OSRSStats();
+        }
+    }
+    public static class WrapperHelpers
+    {
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (T element in source)
+            {
+                action(element);
+            }
         }
     }
 }
--- OSRSStats.cs
+++ OSRSStats.cs
@@ -9,6 +9,13 @@ namespace osrs_hiscore_wrapper
         public List<OSRSLevel> Levels { get; set; }
         public List<OSRSActivity> Activities { get; set; }
         public List<OSRSBoss> Bosses { get; set; }
+
+        public OSRSStats()
+        {
+            Levels = new List<OSRSLevel>();
+            Activities = new List<OSRSActivity>();
+            Bosses = new List<OSRSBoss>();
+        }
     }
     public class OSRSLevel
     {
@@ -17,28 +24,52 @@ namespace osrs_hiscore_wrapper
         private string StatLevel { get; set; }
         private string StatExperience { get; set; }
 
-        public OSRSLevel ToOSRSLevel(string skill, string[] items)
+        public OSRSLevel ToOSRSLevel(string skill, string items)
         {
+            var skills = items.Split(',');
             return new OSRSLevel
             {
                 StatName = skill,
-                StatRank = items[0],
-                StatLevel = items[1],
-                StatExperience = items[2]
+                StatRank = skills[0],
+                StatLevel = skills[1],
+                StatExperience = skills[2]
             };
+        }
     }
 
     public class OSRSBoss
     {
         private string BossName { get; set; }
-        private int BossKC { get; set; }
-        private int BossRank { get; set; }
+        private string BossKC { get; set; }
+        private string BossRank { get; set; }
+
+        public OSRSBoss ToOSRSBoss(string name, string items)
+        {
+            var skills = items.Split(',');
+            return new OSRSBoss
+            {
+                BossName = name,
+                BossRank = skills[0],
+                BossKC = skills[1]
+            };
+        }
     }
 
     public class OSRSActivity
     {
         public string ActivityName { get; set; }
-        public int ActivityKC { get; set; }
-        public int ActivityRank { get; set; }
+        public string ActivityScore { get; set; }
+        public string ActivityRank { get; set; }
+
+        public OSRSActivity ToOSRSActivity(string skill, string items)
+        {
+            var skills = items.Split(',');
+            return new OSRSActivity
+            {
+                ActivityName = skill,
+                ActivityRank = skills[0],
+                ActivityScore = skills[1]
+            };
+        }
     }
 }
